1e092566c94223e4856f0e8dbb647cbd95577dd0
Srinidhi-SA <Srinidhi.Anantharamu@gmail.com>
2021-05-06 10:59:32 +0530

    Test push

---
+++ SPARK_DOCKER/code/mAdvisor-api/copyHadoopImageOldProd.sh
@@ -0,0 +1,6 @@
+cd hadoop_docker
+docker build -t hadoop .
+docker tag hadoop:latest 210201387539.dkr.ecr.us-east-2.amazonaws.com/madvisor-oldprod-hadoop:latest
+$(aws ecr get-login --region us-east-2 --no-include-email) 
+docker push 210201387539.dkr.ecr.us-east-2.amazonaws.com/madvisor-oldprod-hadoop:latest
+cd ..
