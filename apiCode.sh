459a10628567ee91e510dba8b3d7cc0776aa79d4
Srinidhi-SA <Srinidhi.Anantharamu@gmail.com>
2021-05-24 14:37:53 +0530

    AutoML Mail trigger config fixes

--- apiCode.sh
+++ apiCode.sh
@@ -1,10 +1,9 @@
 #! /bin/sh
-rm -r SPARK_DOCKER/marlabs_bi_jobs-0.0.0-py3.6.egg
 rm -r dist
 mkdir dist/
 python3 setup.py bdist_egg
 ls -l dist/
-cp -r dist/marlabs_bi_jobs-0.0.0-py3.6.egg SPARK_DOCKER/
+cp -r dist/* SPARK_DOCKER/marlabs_bi_jobs-0.0.0-py3.6.egg
 ls -l SPARK_DOCKER/
 cd API-CODE
 rm -r mAdvisorProdApiUi
